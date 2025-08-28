db59a805b022f8f067af1b786285e588fc18b115
musire <mcmusire@gmail.com>
2025-08-28 12:10:38 -0600

    Fix styling guidelines

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -3,11 +3,11 @@ import { Outlet } from "react-router-dom";
 const MainLayout = () => {
     return ( 
         <main className="w-screen h-[100dvh] flex-col flex">
-            <nav className="w-full h-20 centered bg-sky-200">navbar</nav>
+            <nav className="w-full h-20 centered bg-teal-200">navbar</nav>
             <div className="grow">
                 <Outlet />
             </div>
-            <footer className="w-full h-20 centered bg-sky-600">footer</footer>
+            <footer className="w-full h-20 centered bg-teal-300">footer</footer>
         </main>
      );
 }
