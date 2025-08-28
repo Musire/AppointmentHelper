fe89f9c3709133111c60a35f33765d9825fe20e6
musire <mcmusire@gmail.com>
2025-08-28 13:27:23 -0600

    initial draft of login page

---
+++ src/pages/login.jsx
@@ -0,0 +1,39 @@
+import { Form, Input } from "@/components";
+import z from "zod";
+
+const LoginPage = () => {
+    const initial = {
+            username: '',
+            password: ''
+        }
+    const loginSchema = z.object({
+        username: z.string().min(1, { message: "Username is required" }),
+        password: z.string().min(1, { message: "Password is required" }),
+    });
+
+    const handleSubmit = (data) => {
+        console.log(data)
+    }
+    return ( 
+        <div className=" w-full h-full p-4">
+            <h2 className="text-xl text-center w-full text-main">Login Page</h2>
+            <Form
+                initialValues={initial}
+                schema={loginSchema}
+                onSubmit={handleSubmit}
+            >
+                <Input
+                    label="username"
+                    name="username"
+                />
+                <Input
+                    type="password"
+                    label="password"
+                    name="password"
+                />            
+            </Form>
+        </div>
+     );
+}
+ 
+export default LoginPage;
