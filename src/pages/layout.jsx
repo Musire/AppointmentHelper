7cc9bf58d662d8868d3ca72712750a3fc5487106
musire <mcmusire@gmail.com>
2025-08-29 09:35:37 -0600

    changed the colors of the navbar and footer for the layout example

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -3,11 +3,11 @@ import { Outlet } from "react-router-dom";
 const MainLayout = () => {
     return ( 
         <main className="w-screen h-[100dvh] flex-col flex">
-            <nav className="w-full h-20 centered bg-teal-200">navbar</nav>
+            <nav className="w-full h-20 centered bg-mid">navbar</nav>
             <div className="grow">
                 <Outlet />
             </div>
-            <footer className="w-full h-20 centered bg-teal-300">footer</footer>
+            <footer className="w-full h-20 centered bg-mid">footer</footer>
         </main>
      );
 }
